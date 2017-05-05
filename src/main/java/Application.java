89cf9e9ab5f87f836353a86e72181a6bfd213d11
gmazzei <gmazzei91@gmail.com>
2017-05-05 20:15:12 -0300

    Cambios para ser deployable en tomcat

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,12 +2,19 @@ package main.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("applicationContext.xml")
-public class Application {
-
+@ImportResource("classpath:applicationContext.xml")
+public class Application extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
